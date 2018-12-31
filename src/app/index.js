428ecfc02d7a3d6cc802b700e5553fb3a7c916ed
Vaibhav sharma <vaibhav.sharma@cgt.co.in>
2018-12-31 16:14:57 +0530

    Redux Middleware

--- src/app/index.js
+++ src/app/index.js
@@ -78,14 +78,20 @@
 
 import { render } from "react-dom";
 import React from "react";
-import { createStore } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from 'redux-logger'
 
 const initialStore = {
     result: 1,
     lastvalues: []
 };
 
-const reducer = ( state = initialStore, action ) => {
+const initialUser = {
+    name: "Vaibhav",
+    age:30
+};
+
+const mathReducer = ( state = initialStore, action ) => {
 
     switch (action.type) {
 
@@ -111,8 +117,44 @@ const reducer = ( state = initialStore, action ) => {
 
 };
 
+const userReducer = ( state = initialUser, action ) => {
 
-const store = createStore(reducer);
+    switch (action.type) {
+
+        case "Set_Name":
+            state = {
+
+                name: action.payload,
+                age: state.age
+            };
+            break;
+
+        case "Set_Age":
+            state = {
+                
+                name: state.name,                
+                age: action.payload
+            };
+            break;
+    }
+
+    return state;
+
+};
+
+// const myLogger = (store) => (next) => (action) => {
+//     console.log("Logged action: ",action);
+//     next(action);
+// };
+const myLogger = createLogger({
+    // console.log("Logged action: ",action),
+    // next(action)
+    diff: true
+});
+
+const store = createStore(combineReducers({mathReducer, userReducer}),{}, 
+    applyMiddleware(myLogger));
+
 
 store.subscribe(() => {
     console.log("Store Updated!", store.getState());
@@ -130,10 +172,18 @@ store.dispatch({
 
 store.dispatch({
     type: "SUBTRACT",
-    payload: 80
+    payload: 22
 });
 
+store.dispatch({
+    type: "Set_Name",
+    payload: "Vaibhsa"
+});
 
+store.dispatch({
+    type: "Set_Age",
+    payload: 31
+});
 
 
 
